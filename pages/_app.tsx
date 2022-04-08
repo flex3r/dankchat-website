0782a7ce24fd800bb2289ac039acd1ae452a774e
Felix <felixschmidbb@gmail.com>
2022-04-08 17:31:15 +0200

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type {AppProps} from 'next/app'
+import {ThemeProvider} from 'next-themes'
+
+function MyApp({Component, pageProps}: AppProps) {
+    return <ThemeProvider>
+        <Component {...pageProps} />
+    </ThemeProvider>
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>DankChat</title>
+        <meta name="description" content="FeelsDankMan" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <a href="https://github.com/flex3r/dankchat">DankChat</a>
+        </h1>
+
+        <p className={styles.description}>
+            <Image src={"/gtKk7.png"} width={500} height={500}/>
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/flex3r/dankchat"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
